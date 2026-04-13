327f7f6eca37a9ff8feec3a69016060c63a44bf6
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-04-13 11:28:55 +0200

    chat functions

--- 0_6_pydanticai_fastapi_chatbot/chat_agents.py
+++ 0_6_pydanticai_fastapi_chatbot/chat_agents.py
@@ -1,6 +1,7 @@
 from pydantic_ai import Agent 
 from dotenv import load_dotenv
 from constants import MODEL_SMALL, MODEL_MEDIUM, MODEL_LARGE
+from data_models import ChatRequest , ChatResponse
 
 # Overrides eventual chaching of environment variables by .env file
 load_dotenv(override=True)
@@ -10,3 +11,11 @@ agent = Agent(
     system_prompt="Be a joking programming nerd, always answer with a programming joke. Also add in some emojis to make it funnier.",
     retries=1
 )
+
+async def chat(request: ChatRequest):
+    result = await agent.run(request.question, message_history=request.message_history)
+
+    return ChatResponse(
+        response=result.output,
+        message_history=result.all_messages()
+    )
